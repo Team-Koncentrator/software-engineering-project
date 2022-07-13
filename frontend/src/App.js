006a700f9bae1160022338f798ddb22f284cf0f6
Pacuch <michal.patz@interia.pl>
2022-07-13 21:35:50 +0200

    essares

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn essar
+          Learn essares
         </a>
       </header>
     </div>
