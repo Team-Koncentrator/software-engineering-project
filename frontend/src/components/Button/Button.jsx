fb88e08a0a715907339eb14090959f414cd64860
Paweł Biniak <pawel.biniak@reckitt.com>
2022-09-18 20:56:52 +0200

    button change

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Button.css';
 
-export default function Button({ name }) {
+export default function Button(props) {
+  const { value, clickHandler, keyDownHandler, type } = props;
+
   return (
     <div>
-      <button className='myButton'>{name}</button>
+      <button className='myButton' onClick={clickHandler} onKeyDown={keyDownHandler} type={type}>
+        {value}
+      </button>
     </div>
   );
 }
+
+Button.defaultProps = {
+  type: 'button'
+};
