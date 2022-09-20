34c750a42d8fb8c1def8030d9524d5f2a1a8c0c0
Pacuch <michal.patz@interia.pl>
2022-09-20 22:36:47 +0200

    added register with validation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,8 @@ import 'App.css';
 import Navigation from 'components/Navigation/Navigation';
 import GlobalStyle from 'theme/globalStyles';
 import Home from 'pages/Home/Home';
-import Login from 'pages/Login/Login';
+//import Login from 'pages/Login/Login';
+import Register from 'pages/Register/Register';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='login' element={<Login />} />
+        <Route path='login' element={<Register />} />
       </Routes>
     </>
   );
