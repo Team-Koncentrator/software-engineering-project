6df2913b965c7cf7f9009ea7bfa5870db2a7e8c1
Paweł Biniak <pawel.biniak@reckitt.com>
2022-09-26 20:38:56 +0200

    merge commit

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,17 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 
+const db = mongoose.connection;
+mongoose.connect("127.0.0.1");
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
 
-const db = mongoose.connection
-mongoose.connect(process.env.DATABASE_URL)
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to database'))
+const app = express();
+app.use(express.json()); // accept json in header
 
-const app = express()
-app.use(express.json())     // accept json in header
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
 
-const usersRouter = require('./routes/users')
-app.use('/users', usersRouter)
-
-app.listen(3000, () => console.log('Server started at port 3000'))
-
+app.listen(3000, () => console.log("Server started at port 3000"));
