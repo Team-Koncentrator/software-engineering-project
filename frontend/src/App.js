f915ee5247713e0342735ccd8b357fcb1260d9ed
Wojciech Witczak <70755745+wojtkolos@users.noreply.github.com>
2022-09-27 20:53:17 +0200

    first-commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,12 @@ import 'App.css';
 import Navigation from 'components/Navigation/Navigation';
 import GlobalStyle from 'theme/globalStyles';
 import Home from 'pages/Home/Home';
-//import Login from 'pages/Login/Login';
+import Login from 'pages/Login/Login';
 import Register from 'pages/Register/Register';
+
 import Faq from 'pages/Faq/Faq';
 import Authors from 'pages/Authors/Authors';
+import Assign from 'components/dnd/example';
 
 function App() {
   return (
@@ -16,14 +18,11 @@ function App() {
       <Navigation />
       <Routes>
         <Route path='/' element={<Home />} />
-<<<<<<< HEAD
-        <Route path='login' element={<Register />} />
-=======
         <Route path='register' element={<Register />} />
         <Route path='login' element={<Login />} />
         <Route path='faq' element={<Faq />} />
         <Route path='authors' element={<Authors />} />
->>>>>>> work
+        <Route path='assign' element={ <Assign />} />
       </Routes>
     </>
   );
