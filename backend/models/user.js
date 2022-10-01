29321900df08ab600b254257c90e52224999404f
Maciej Węgrzak <m.wegrzak@gmail.com>
2022-10-01 16:13:10 +0200

    Added password field in db

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,23 +3,18 @@ const mongoose = require("mongoose");
 const usersSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: true,
   },
   lastName: {
     type: String,
-    required: true,
   },
   age: {
     type: Number,
-    required: true,
   },
   gender: {
     type: String,
-    required: true,
   },
   password: {
     type: String,
-    required: true,
   },
 });
 
